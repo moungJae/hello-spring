52b0ad5401865080a78719bfcecb7fe1e8cd44b5
moungjae joe <cmj092222@naver.com>
2022-01-14 18:46:39 +0900

    JPA commit

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,7 +1,13 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
     private String name;
 
     public String getName() {
