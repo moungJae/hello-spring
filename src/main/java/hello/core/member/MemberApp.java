ad2404e91e5d40e19878459fcdbc5ffa826aa96e
moungjae joe <cmj092222@naver.com>
2022-01-25 01:31:51 +0900

    spring, spring bean practice

--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,13 +1,19 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     // psvm : 자동으로 public static void main 생성
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         // ctrl + alt + v : 자동으로 Member member 생성
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
