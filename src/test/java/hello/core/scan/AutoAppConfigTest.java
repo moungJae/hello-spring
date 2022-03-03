118a9b2e0aebbf1b69384b9e7f0910e5624fa84a
moungjae joe <cmj092222@naver.com>
2022-03-03 18:05:29 +0900

    Component scan and Filter practice

---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,24 @@
+package hello.core.scan;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.order.OrderService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Member;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
